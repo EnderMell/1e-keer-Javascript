7a2d6bcaddd4bb6bc02665fe58cd75b711605cf8
EnderMell <220813@student.glu.nl>
2022-12-23 13:17:46 +0100

    first commit

---
+++ workshop 1/assets/js/script.js
@@ -0,0 +1,24 @@
+var voorNaam = 'Michelle';
+var achterNaam = 'Tuinbeek';
+var voledigeNaam = (voorNaam + achterNaam);
+var geboortedag = "25 -"; 
+var geboortemaand = " 06 -";
+var geboortejaar = " 2005"; 
+var huidig_jaartal = "2022"
+var leeftijd = 17; 
+var dag_van_het_jaar = 340; 
+var straat = "Oostzee "; 
+var huisnummer = "10 "; 
+var woonplaats = "Vathorst "; 
+var woonachtig_in_utrecht = ""; // check hier dmv een boolean of jouw woonplaats gelijk is aan "Utrecht"
+var aantal_studenten_in_deze_klas = 21;
+var kosten_van_frikandelbroodje = 1.76;
+var totale_lunchkosten = 0;
+
+document.write('Voornaam: ' +  voorNaam);
+document.write("<br>")
+document.write('Achternaam: ' +  achterNaam);
+document.write("<br>")
+document.write('Geboorte Datum: ' + geboortedag + geboortemaand + geboortejaar);
+document.write("<br>")
+document.write('Woonplaats: '+ woonplaats + straat + huisnummer  );
---
+++ workshop 2/assets/js/script.js
@@ -0,0 +1,43 @@
+document.getElementById("a").onclick = function(){ 
+	var a_toets = document.getElementById("audio_a"); 
+	a_toets.play(); 
+}
+document.getElementById("b").onclick = function(){
+	var b_toets = document.getElementById("audio_b"); 
+	b_toets.play();
+}
+document.getElementById("c").onclick = function(){ // als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_c"); // zoek het c-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_d");
+	d_toets.play();
+}
+document.getElementById("e").onclick = function(){
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+}
+document.getElementById("f").onclick = function(){
+	var f_toets = document.getElementById("audio_f");
+	f_toets.play();
+}
+document.getElementById("g").onclick = function(){
+	var g_toets = document.getElementById("audio_g");
+	g_toets.play();
+}
+
+document.getElementById("ar").onclick = function(){
+    var ar_toets = document.getElementById("audio_ar");
+    ar_toets.play();
+}
+
+document.getElementById("br").onclick = function(){
+    var br_toets = document.getElementById("audio_br");
+    br_toets.play();
+}
+
+document.getElementById("cr").onclick = function(){
+    var cr_toets = document.getElementById("audio_cr");
+    cr_toets.play();
+}
